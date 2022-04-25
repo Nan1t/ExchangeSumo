8879c459062d0ab88ae4cb42e9b3cb80845199c5
Nanit <nanitmix@gmail.com>
2022-04-25 19:18:44 +0300

    Moved terms to localized strings

--- app/src/main/java/ua/nanit/exsumo/ui/TermsDialog.kt
+++ app/src/main/java/ua/nanit/exsumo/ui/TermsDialog.kt
@@ -1,6 +1,8 @@
 package ua.nanit.exsumo.ui
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import ua.nanit.exsumo.R
 import ua.nanit.exsumo.storage.AppStorage
@@ -9,21 +11,16 @@ import kotlin.system.exitProcess
 class TermsDialog {
 
     fun show(ctx: Context, storage: AppStorage? = null) {
-        val text = ctx.resources
-            .openRawResource(R.raw.terms_and_conditions)
-            .reader()
-            .readText()
-
         val builder = MaterialAlertDialogBuilder(ctx)
         builder.setTitle(R.string.about_terms)
-        builder.setMessage(text)
-        builder.setPositiveButton(R.string.ok) { _, _ ->
+        builder.setMessage(R.string.terms_text)
+        builder.setPositiveButton(R.string.terms_ok) { _, _ ->
             storage?.acceptTerms()
         }
-        if (storage != null) {
-            builder.setNegativeButton(R.string.cancel) { _, _ ->
-                exitProcess(0)
-            }
+        builder.setNegativeButton(R.string.terms_website) { _, _ ->
+            val url = ctx.resources.getString(R.string.about_partner_url)
+            ctx.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+            exitProcess(0)
         }
         builder.create().show()
     }
